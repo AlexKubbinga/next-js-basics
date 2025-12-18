8fcb785c116f8f08049cb1fe13633bf3e9bbe615
Alex Kubbinga <63915427+AlexKubbinga@users.noreply.github.com>
2025-12-18 07:42:00 +0000

    added streaming on a component level and added a wrapper for the card components

--- app/ui/dashboard/revenue-chart.tsx
+++ app/ui/dashboard/revenue-chart.tsx
@@ -8,12 +8,12 @@ import { Revenue } from '@/app/lib/definitions';
 // https://www.tremor.so/
 // https://www.chartjs.org/
 // https://airbnb.io/visx/
+import { Suspense } from 'react';
+import { RevenueChartSkeleton } from '@/app/ui/skeletons';
+import { fetchRevenue } from '@/app/lib/data';
 
-export default async function RevenueChart({
-  revenue,
-}: {
-  revenue: Revenue[];
-}) {
+export default async function RevenueChart() {
+  const revenue = await fetchRevenue();
   const chartHeight = 350;
   // NOTE: Uncomment this code in Chapter 7
 
